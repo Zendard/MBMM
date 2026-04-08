187bf9453ec0421ff57f4f098071c14c1f1fb5dd
Zendard <thijsmuylaert@hotmail.com>
2026-04-08 22:02:06 +0200

    Fix preferences loading and saving

--- src/preferences.rs
+++ src/preferences.rs
@@ -1,21 +1,33 @@
 use field_count::FieldCount;
 use std::{error::Error, path::PathBuf};
 
-#[derive(FieldCount)]
+#[derive(FieldCount, Clone, Debug)]
 pub struct Preferences {
     game_dir: PathBuf,
 }
 
 impl Preferences {
     pub fn load() -> Option<Self> {
-        let file_path = dirs::config_dir()?;
+        let mut file_path = dirs::config_dir()?;
+        file_path.push(PathBuf::from("MBMM"));
+        file_path.push(PathBuf::from("preferences"));
+
         let file = std::fs::read_to_string(file_path).ok()?;
         Preferences::decode(&file).ok()
     }
 
     pub fn save(&self) -> Result<(), Box<dyn Error>> {
         let string = self.encode()?;
-        let file_path = dirs::config_dir().ok_or("Can't get config_dir")?;
+        let mut file_path = dirs::config_dir().ok_or("Can't get config_dir")?;
+
+        file_path.push(PathBuf::from("MBMM"));
+        // Make config dir if it doesn't exist
+        if !file_path.try_exists().unwrap_or(false) {
+            std::fs::create_dir(&file_path)?
+        }
+
+        file_path.push(PathBuf::from("MBMM"));
+
         std::fs::write(file_path, string)?;
         Ok(())
     }
